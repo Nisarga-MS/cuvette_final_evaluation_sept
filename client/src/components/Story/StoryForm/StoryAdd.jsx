e96f05025a3db0ddc5dc818d66c2efff706642c7
Nisarga-MS <84907841+Nisarga-MS@users.noreply.github.com>
2024-04-25 20:51:54 +0530

    added  all components related to story

---
+++ client/src/components/Story/StoryForm/StoryAdd.jsx
@@ -0,0 +1,249 @@
+import React, { useEffect, useState } from "react";
+import styles from "./StoryForm.module.css";
+import Button from "../../Button/Button";
+import SlideForm from "./SlideForm";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { ADD_STORY } from "../../../globals";
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal } from "../../../redux/modal/modalSlice";
+import {
+  addStoryRequest,
+  addStorySuccess,
+  addStoryFailure,
+} from "../../../redux/story/storySlice";
+
+const StoryForm = () => {
+  const dispatch = useDispatch();
+  const { modalContent } = useSelector((state) => state.modal);
+  const { user } = useSelector((state) => state.userauth);
+  const { isSmallScreen } = useSelector((state) => state.layout);
+
+  const initialSlide = {
+    heading: "",
+    description: "",
+    imageUrl: "",
+    category: "",
+  };
+
+  const [slides, setSlides] = useState([
+    initialSlide,
+    initialSlide,
+    initialSlide,
+  ]);
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    setCurrentSlide(currentSlide);
+    console.log("currentSlide", currentSlide);
+  }, [currentSlide]);
+
+  useEffect(() => {
+    if (slides.length > 6) {
+      alert("please remove slides");
+    }
+    if (slides.length < 3) {
+      alert("please add slides");
+    }
+  }, [slides]);
+
+  const handleValidate = (name, value) => {
+    if (name === "category" && value === "") {
+      setError("Please select a category");
+    } else if (name === "imageUrl" && value == "") {
+      setError("Please add an image url");
+    } else if (name === "description" && value == "") {
+      setError("Please add a description");
+    } else if (name === "heading" && value == "") {
+      setError("Please add a heading");
+    } else {
+      setError("");
+    }
+  };
+
+  const handleChange = (e, index) => {
+    const { name, value } = e.target;
+    handleValidate(name, value);
+    setSlides((prevSlides) =>
+      prevSlides.map((slide, i) =>
+        i === index ? { ...slide, [name]: value } : slide
+      )
+    );
+  };
+
+  const handleSubmit = async () => {
+    const { VITE_BACKEND_URL } = import.meta.env;
+
+    try {
+      const isValid = slides.some((slide, index) => {
+        if (
+          Object.keys(slide).length === 0 ||
+          slide.heading?.trim() === "" ||
+          slide.description?.trim() === "" ||
+          slide.imageUrl?.trim() === "" ||
+          slide.category?.trim() === ""
+        ) {
+          setError(slide, index);
+        }
+        return (
+          Object.keys(slide).length === 0 ||
+          slide.heading?.trim() === "" ||
+          slide.description?.trim() === "" ||
+          slide.imageUrl?.trim() === "" ||
+          slide.category?.trim() === ""
+        );
+      });
+
+      if (isValid) {
+        setError("Please fill out all fields");
+        return;
+      }
+
+      if (slides.length < 3) {
+        setError("Please add at least 3 slides");
+        return;
+      } else if (slides.length > 6) {
+        setError("Please remove slides");
+        return;
+      }
+
+      dispatch(addStoryRequest());
+
+      const response = await axios
+        .post(`${VITE_BACKEND_URL}/api/story/add`, {
+          slides,
+          addedBy: user,
+        })
+        .then((response) => {
+          if (response.data.success) {
+            toast.success("Story added successfully", {
+              position: "top-center",
+            });
+          }
+          dispatch(addStorySuccess());
+          dispatch(closeModal());
+        });
+    } catch (error) {
+      dispatch(addStoryFailure());
+      toast.error("Error adding story", { position: "top-center" });
+    }
+  };
+
+  const handleAddSlide = () => {
+    if (slides.length < 6) {
+      setSlides((prevSlides) => [...prevSlides, {}]);
+      setCurrentSlide(slides.length);
+    }
+  };
+
+  const handleRemoveSlide = (index) => {
+    if (slides && slides.length > 3) {
+      setSlides((prevSlides) => prevSlides.filter((_, i) => i !== index));
+      handlePrevClick();
+    }
+  };
+
+  const handleClose = () => {
+    dispatch(closeModal());
+  };
+
+  const handlePrevClick = () => {
+    setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 0);
+  };
+  const handleNextClick = () => {
+    setCurrentSlide(
+      currentSlide < slides.length - 1 ? currentSlide + 1 : slides.length - 1
+    );
+  };
+  return (
+    <div
+      className={`${styles.story_form} ${
+        isSmallScreen ? styles.story_form__small : ""
+      }`}
+      style={{ display: modalContent === ADD_STORY ? "flex" : "none" }}
+    >
+      <div className={styles.storySlide__container}>
+        {slides.map((slide, index) => (
+          <div
+            className={styles.slide_box}
+            onClick={() => setCurrentSlide(index)}
+            style={{
+              border: currentSlide === index ? "2px solid #73ABFF" : "none",
+            }}
+          >
+            slide {index + 1}
+          </div>
+        ))}
+        <div
+          className={styles.slide_box}
+          onClick={handleAddSlide}
+          style={{ cursor: "pointer" }}
+        >
+          Add +
+        </div>
+      </div>
+      {/* close icon */}
+      <svg
+        className={styles.story_close}
+        onClick={handleClose}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12 0C5.38341 0 0 5.38341 0 12C0 18.6166 5.38341 24 12 24C18.6166 24 24 18.6166 24 12C24 5.38341 18.6166 0 12 0ZM12 1.84615C17.6178 1.84615 22.1538 6.38221 22.1538 12C22.1538 17.6178 17.6178 22.1538 12 22.1538C6.38221 22.1538 1.84615 17.6178 1.84615 12C1.84615 6.38221 6.38221 1.84615 12 1.84615ZM8.50962 7.18269L7.18269 8.50962L10.6731 12L7.18269 15.4904L8.50962 16.8173L12 13.3269L15.4904 16.8173L16.8173 15.4904L13.3269 12L16.8173 8.50962L15.4904 7.18269L12 10.6731L8.50962 7.18269Z"
+          fill="#FF0000"
+        />
+      </svg>
+      {/* slide form */}
+      <div className={styles.SlideForm__container}>
+        {slides.map((slide, slideIndex) => (
+          <>
+            {slideIndex === currentSlide && (
+              <SlideForm
+                key={slideIndex}
+                slide={slide}
+                slideIndex={slideIndex}
+                handleChange={(e) => handleChange(e, slideIndex)}
+                handleRemoveSlide={() => handleRemoveSlide(slideIndex)}
+              />
+            )}
+          </>
+        ))}
+      </div>
+      <span className={styles.form_err}>{error}</span>
+
+      <div className={styles.buttons}>
+        <Button
+          myFunction={handlePrevClick}
+          color="#7eff73"
+          text="Previous"
+          size="small"
+        ></Button>
+        <Button
+          myFunction={handleNextClick}
+          color="#73abff"
+          text="Next"
+          size="small"
+        ></Button>
+        {slides.length > 3 ? (
+          <Button
+            myFunction={() => handleRemoveSlide(currentSlide)}
+            color="#ff73b5"
+            text="Remove"
+            size="small"
+          ></Button>
+        ) : (
+          <div style={{ width: "30%" }}></div>
+        )}
+        <Button myFunction={handleSubmit} text="Post" size="small"></Button>
+      </div>
+    </div>
+  );
+};
+
+export default StoryForm;
